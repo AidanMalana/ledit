829d918857aba30159bcbb0cdab20ed04e2ae0b5
Aidan Malana <aidanmalana02@gmail.com>
2024-12-31 10:47:09 -0600

    Initial Project

---
+++ build/CMakeFiles/ledit.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ledit.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/ledit.dir/main.c.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/ledit.dir/main.c.o: /home/amm/Projects/ledit/main.c \
+ /usr/include/stdc-predef.h /usr/local/include/raylib.h \
+ /usr/lib/gcc/x86_64-linux-gnu/12/include/stdarg.h \
+ /usr/lib/gcc/x86_64-linux-gnu/12/include/stdbool.h
