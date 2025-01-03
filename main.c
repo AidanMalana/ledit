a0295275308313764ed39c2e25b6aac35c25c7c1
Aidan Malana <aidanmalana02@gmail.com>
2025-01-02 22:25:02 -0600

    Add ability to resize map using GUI

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+// CURRENT ERRORS/BUGS
+// HANDLE CASES WHERE MEMORY ALLOCATION MAY RETURN NULL
 #include "raylib.h"
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
@@ -16,6 +18,8 @@ typedef struct {
 void UpdateDrawFrame(void);     // Update and Draw one frame
 void DrawTilemap(RenderTexture2D, Tile*);         // Draw the tilemap
 void DrawMouse(RenderTexture2D); // Draw the mouse 
+void saveMap();
+void resizeTileMap();
 
 //----------------------------------------------------------------------------------
 // Global Variables Definition
@@ -42,6 +46,13 @@ int worldHeight = 10;
 Tile *tilemap; 
 FILE *mapFile;
 char *mapFileName;
+
+// GUI
+bool showResizeWorldPanel = false;
+int selectedWorldWidth;
+int selectedWorldHeight;
+bool widthBoxSelected;
+bool heightBoxSelected;
 
 
 //----------------------------------------------------------------------------------
@@ -76,6 +87,10 @@ int main()
             }
             fclose(mapFile);
             mapFile = NULL;
+        } else {
+            mapFile = fopen("levels/level.txt", "r");
+            fscanf(mapFile, "%d %d %d", &worldWidth, &worldHeight, &tileSize);
+            fclose(mapFile);
         }
         strcpy(mapFileName, "levels/level.txt");
     } else {
@@ -92,7 +107,7 @@ int main()
         mapFile = NULL;
     }
     mapFile = fopen(mapFileName, "r");
-    tilemap = calloc(100, sizeof(Tile));
+    tilemap = calloc(worldHeight * worldWidth, sizeof(Tile));
     char line[1024];
     int lineCount = 0;
     int tmpX, tmpY, tmpZ;
@@ -100,8 +115,9 @@ int main()
         if (lineCount > 0) {
             // I think the better way to do this would be to use JSON or something, or at the very least use strtol, but I just want to try this for now
             sscanf(line, "%d %d %d", &tmpX, &tmpY, &tmpZ); // X and Y will need to be in units of tiles not pixels
-            tilemap[(int)tmpX * worldHeight + (int)tmpY].x = tmpX * tileSize;
-            tilemap[(int)tmpX * worldHeight + (int)tmpY].y = tmpY * tileSize;
+            // I never actually use the x or y for anything (hopefully)
+            tilemap[(int)tmpX * worldHeight + (int)tmpY].x = tmpX;
+            tilemap[(int)tmpX * worldHeight + (int)tmpY].y = tmpY;
             tilemap[(int)tmpX * worldHeight + (int)tmpY].rect.x = tmpX * tileSize;
             tilemap[(int)tmpX * worldHeight + (int)tmpY].rect.y = tmpY * tileSize;
             tilemap[(int)tmpX * worldHeight + (int)tmpY].rect.width = tileSize;
@@ -110,22 +126,11 @@ int main()
         }
         lineCount++;
     }
-    // for (int i = 0; i < worldWidth; i++) {
-    //     for (int j = 0; j < worldHeight; j++) {
-    //         tilemap[i * worldHeight + j].x = i * tileSize;
-    //         tilemap[i * worldHeight + j].y = j * tileSize;
-    //         tilemap[i * worldHeight + j].rect.x = i * tileSize;
-    //         tilemap[i * worldHeight + j].rect.y = j * tileSize;
-    //         tilemap[i * worldHeight + j].rect.width = tileSize;
-    //         tilemap[i * worldHeight + j].rect.height = tileSize;
-    //         tilemap[i * worldHeight + j].textureRec = tileset[2];
-    //     }
-    // }
     fclose(mapFile);
     mapFile = NULL;
 
     SetTargetFPS(60);   // Set our game to run at 60 frames-per-second
-
+    SetExitKey(KEY_Q);
     //--------------------------------------------------------------------------------------
 
     // Main game loop
@@ -168,24 +173,27 @@ int main()
                 break;
         }
 
-        // Save map to file 
-        if (IsKeyDown(KEY_S)) {
-            mapFile = fopen(mapFileName, "w");
-            fprintf(mapFile, "%d %d %d\n", worldWidth, worldHeight, tileSize);
-            for (int i = 0; i < worldWidth; i++) {
-                for (int j = 0; j < worldHeight; j++) {
-                    fprintf(mapFile, "%d %d %d\n", i, j, tilemap[i * worldHeight + j].tile);
-                }
-            }
-            fclose(mapFile);
-            mapFile = NULL;
+        // Show resize panel
+        if (IsKeyPressed(KEY_ESCAPE) && !showResizeWorldPanel) {
+            showResizeWorldPanel = true;
+            selectedWorldWidth = worldWidth;
+            selectedWorldHeight = worldHeight;
+            widthBoxSelected = false;
+            heightBoxSelected = false;
+            ShowCursor();
+        }
 
+        // Save map to file 
+        if (IsKeyPressed(KEY_S)) {
+            saveMap();
         }
         if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
             Vector2 mousePos = GetMousePosition();
             int x = round((mousePos.x)/scale/tileSize);
             int y = round((mousePos.y)/scale/tileSize);
-            tilemap[x * worldHeight + y].tile = selectedTile;
+            if (x < worldWidth && y < worldHeight) {
+                tilemap[x * worldHeight + y].tile = selectedTile;
+            }
         }
         UpdateDrawFrame();
     }
@@ -240,6 +248,34 @@ void UpdateDrawFrame(void)
             (Vector2) {0, 0}, 
             0.0f,
             WHITE);
+        
+        if (showResizeWorldPanel) {
+            Rectangle windowBox = (Rectangle) {screenWidth/2 - 160, screenHeight/2 - 90, 320, 180};
+            int result = GuiWindowBox(windowBox, "Resize World");
+            if (result > 0) showResizeWorldPanel = false; // If window close button clicked
+            /*Seems to work, but I need to add to the map.
+            Currently, if I set it higher than the current width stored in the level file, it crashes, due to what I assume is an indexOutOfBounds thing. 
+            */ 
+            Rectangle widthBox = (Rectangle) {windowBox.x + 48, windowBox.y + 48, 248, 24};
+            Rectangle heightBox = (Rectangle) {windowBox.x + 48, windowBox.y + 96, 248, 24};
+
+            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                widthBoxSelected = false;
+                heightBoxSelected = false;
+                if (CheckCollisionPointRec(GetMousePosition(), widthBox)) {
+                    widthBoxSelected = true;
+                }
+                if (CheckCollisionPointRec(GetMousePosition(), heightBox)) {
+                    heightBoxSelected = true;
+                }
+            }
+            GuiValueBox(widthBox, "Width: ", &selectedWorldWidth, 1, 100, widthBoxSelected);  
+            GuiValueBox(heightBox, "Height: ", &selectedWorldHeight, 1, 100, heightBoxSelected);  
+            int apply = GuiButton((Rectangle){windowBox.x + 24, windowBox.y + windowBox.height - 48, 64, 24}, "Apply");
+            if (apply == 1) {
+                resizeTileMap();
+            }
+        }
 
     EndDrawing();
     //----------------------------------------------------------------------------------
@@ -261,4 +297,102 @@ void DrawMouse(RenderTexture2D target) {
     Vector2 mousePos = GetMousePosition();
     DrawRectangle(round((mousePos.x)/scale/tileSize)*tileSize-1, round((mousePos.y)/scale/tileSize)*tileSize-1, tileSize+2, tileSize+2, YELLOW);
     DrawTextureRec(tilesetTexture, tileset[selectedTile], (Vector2) {round((mousePos.x)/scale/tileSize)*tileSize, round((mousePos.y)/scale/tileSize)*tileSize}, WHITE);
+}
+
+void saveMap() {
+    mapFile = fopen(mapFileName, "w");
+    fprintf(mapFile, "%d %d %d\n", worldWidth, worldHeight, tileSize);
+    for (int i = 0; i < worldWidth; i++) {
+        for (int j = 0; j < worldHeight; j++) {
+            fprintf(mapFile, "%d %d %d\n", i, j, tilemap[i * worldHeight + j].tile);
+        }
+    }
+    fclose(mapFile);
+    mapFile = NULL;
+}
+
+void resizeTileMap()
+{
+    if (selectedWorldWidth <= worldWidth && selectedWorldHeight <= worldHeight) {
+        Tile *newTileMap = calloc(selectedWorldWidth * selectedWorldHeight, sizeof(Tile));
+        for (int i = 0; i < selectedWorldWidth; i++) {
+            for (int j = 0; j < selectedWorldHeight; j++) {
+                newTileMap[i * selectedWorldHeight + j] = tilemap[i * worldHeight + j];
+            }
+        }
+
+        if (tilemap != NULL) {
+            free(tilemap);
+        }
+        tilemap = NULL;
+        tilemap = newTileMap;
+    } else {
+        Tile *newTileMap = calloc(selectedWorldWidth * selectedWorldHeight, sizeof(Tile));
+        for (int i = 0; i < worldWidth; i++) {
+            for (int j = 0; j < worldHeight; j++) {
+                newTileMap[i * selectedWorldHeight + j] = tilemap[i * worldHeight + j];
+            }
+        }
+
+        if (tilemap != NULL) {
+            free(tilemap);
+        }
+        tilemap = NULL;
+
+        /* Cover all cases/tiles. There might be a more effecient solution but this works*/
+        if (selectedWorldWidth > worldWidth && selectedWorldHeight <= worldHeight) {
+            for (int i = worldWidth; i < selectedWorldWidth; i++) {
+                for (int j = 0; j < selectedWorldHeight; j++) {
+                    newTileMap[i * selectedWorldHeight + j].x = i;
+                    newTileMap[i * selectedWorldHeight + j].y = j;
+                    newTileMap[i * selectedWorldHeight + j].rect.x = i * tileSize;
+                    newTileMap[i * selectedWorldHeight + j].rect.y = j * tileSize;
+                    newTileMap[i * selectedWorldHeight + j].rect.width = tileSize;
+                    newTileMap[i * selectedWorldHeight + j].rect.height = tileSize;
+                    newTileMap[i * selectedWorldHeight + j].tile = defaultTile;
+                }
+            }
+        } else if (selectedWorldWidth <= worldWidth && selectedWorldHeight > worldHeight) {
+            for (int i = 0; i < selectedWorldWidth; i++) {
+                for (int j = worldHeight; j < selectedWorldHeight; j++) {
+                    newTileMap[i * selectedWorldHeight + j].x = i;
+                    newTileMap[i * selectedWorldHeight + j].y = j;
+                    newTileMap[i * selectedWorldHeight + j].rect.x = i * tileSize;
+                    newTileMap[i * selectedWorldHeight + j].rect.y = j * tileSize;
+                    newTileMap[i * selectedWorldHeight + j].rect.width = tileSize;
+                    newTileMap[i * selectedWorldHeight + j].rect.height = tileSize;
+                    newTileMap[i * selectedWorldHeight + j].tile = defaultTile;
+                }
+            }
+        } else if (selectedWorldWidth > worldWidth && selectedWorldHeight > worldHeight) { // Doesn't seem to fill all tiles
+            for (int i = worldWidth; i < selectedWorldWidth; i++) {
+                for (int j = 0; j < selectedWorldHeight; j++) {
+                    newTileMap[i * selectedWorldHeight + j].x = i;
+                    newTileMap[i * selectedWorldHeight + j].y = j;
+                    newTileMap[i * selectedWorldHeight + j].rect.x = i * tileSize;
+                    newTileMap[i * selectedWorldHeight + j].rect.y = j * tileSize;
+                    newTileMap[i * selectedWorldHeight + j].rect.width = tileSize;
+                    newTileMap[i * selectedWorldHeight + j].rect.height = tileSize;
+                    newTileMap[i * selectedWorldHeight + j].tile = defaultTile;
+                }
+            }
+            for (int i = 0; i < selectedWorldWidth; i++) {
+                for (int j = worldHeight; j < selectedWorldHeight; j++) {
+                    newTileMap[i * selectedWorldHeight + j].x = i;
+                    newTileMap[i * selectedWorldHeight + j].y = j;
+                    newTileMap[i * selectedWorldHeight + j].rect.x = i * tileSize;
+                    newTileMap[i * selectedWorldHeight + j].rect.y = j * tileSize;
+                    newTileMap[i * selectedWorldHeight + j].rect.width = tileSize;
+                    newTileMap[i * selectedWorldHeight + j].rect.height = tileSize;
+                    newTileMap[i * selectedWorldHeight + j].tile = defaultTile;
+                }
+            }
+        }
+
+        tilemap = newTileMap;
+    }
+
+    worldWidth = selectedWorldWidth;
+    worldHeight = selectedWorldHeight;
+    saveMap();
 }
